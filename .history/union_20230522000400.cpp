e6e5b9d9fcf52a36ef2ce2cf3eb371816f8edc93
Lakshya Singh <lakshyaasin@gmail.com>
2023-05-22 00:07:19 +0530

    union using maps

---
+++ .history/union_20230522000400.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+#include <map>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+    map<int, int> m;
+    for (int i = 0; i < nums1.size(); i++)
+    {
+        m[nums1[i]] = 1;
+    }
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        m[nums2[i]] == 1;
+    }
+    for (int i = 0; i < m.size(); i++)
+    {
+        if (m[i] == 1)
+        {
+            v.push_back(i);
+        }
+    }
+    cout << v.size();
+
+    return 0;
+}
