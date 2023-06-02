c734a1a1255e6788080f4da2ba0a87ecd39a45f8
Lakshya Singh <lakshyaasin@gmail.com>
2023-06-02 17:34:16 +0530

    intersection

--- intersect.cpp
+++ intersect.cpp
@@ -4,30 +4,60 @@
 #include <algorithm>
 #include <map>
 using namespace std;
-int main(){
-    vector<int>v;
-    vector<int>nums1={1,2,2,1};
-    vector<int> nums2 = {2,2};
-    map<int, int> m;
-    for (int i = 0; i < nums1.size(); i++)
+int main()
+{
+    vector<int> v;
+    vector<int> nums1 = {4, 9, 5};
+    vector<int> nums2 = {4, 4, 8, 9, 9};
+    // Brute: using a map space is O(m)
+    // map<int, int> m;
+    // for (int i = 0; i < nums1.size(); i++)
+    // {
+    //     m[nums1[i]]=1;
+    // }
+    // for (int i = 0; i < nums2.size(); i++)
+    // {
+    //     if (m[nums2[i]] == 1)
+    //     {
+    //         m[nums2[i]]=2;
+    //     }
+    // }
+    // for(int i=0;i<m.size();i++){
+    //     if(m[i]==2){
+    //         v.push_back(i);
+    //     }
+    // }
+    // Optimal: since they are sorted, use two pointers like union
+    int i = 0;
+    int j = 0;
+    while (i < nums1.size() && j < nums2.size())
     {
-        m[nums1[i]]=1;
-    }
-    for (int i = 0; i < nums2.size(); i++)
-    {
-        if (m[nums2[i]] == 1)
+        while (i < nums1.size() - 1 && nums1[i] == nums1[i + 1])
         {
-            m[nums2[i]]=2;
+            i++;
+        }
+        while (j < nums2.size() - 1 && nums2[j] == nums2[j + 1])
+        {
+            j++;
+        }
+        if (nums1[i] < nums2[j])
+        {
+            i++;
+        }
+        else if (nums1[i] > nums2[j])
+        {
+            j++;
+        }
+        else
+        {
+            v.push_back(nums1[i]);
+            i++;
+            j++;
         }
     }
-    for(int i=0;i<m.size();i++){
-        if(m[i]==2){
-            v.push_back(i);
-        }
-    }
-    for(int i=0;i<v.size();i++)
+    for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i]<<" ";
+        cout << v[i] << " ";
     }
     return 0;
 }
